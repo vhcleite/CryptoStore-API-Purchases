8503aa50bdb382c4ef026054ed30520822b69fca
Victor Leite <victor_leite@outlook.com.br>
2021-07-14 12:00:56 -0300

    Commit inicial

---
+++ src/test/java/com/store/apipurchases/ApiPurchasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.apipurchases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPurchasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
